424e31d7b53e629bd1b8de367b5274b8c7e05afa
Jakkoble <hey@jakob.foo>
2024-08-09 17:09:39 +0200

    added mobile scroll indicator

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,25 +1,37 @@
+import { Link } from "react-router-dom";
+import ArrowDown from "../assets/svg/arrow-down.svg";
 import Button from "./Button";
 import Highlight from "./Highlight";
 
 const Hero = () => {
 	return (
-		<section className="flex min-h-dvh flex-col items-center justify-end">
-			<div className="mt-32 mb-48 flex min-h-dvh w-min flex-col items-center justify-center text-center sm:min-h-0 sm:justify-normal">
-				<h1 className="pb-6 text-7xl sm:text-8xl">
+		<section className="flex min-h-lvh flex-col items-center justify-end">
+			{/* Hero C2A Section */}
+			<div className="relative flex min-h-svh w-full flex-col items-center justify-center text-center md:min-h-0 md:w-min">
+				<h1 className="pb-6 text-5xl sm:text-8xl">
 					ClassInsights<span className="text-primary">.</span>
 				</h1>
 				<p className="pb-6 text-lg sm:w-8/12">
 					Die innovative Energie-Management Lösung für Schulen auf Basis des
 					Stundenplanes
 				</p>
-				<div className="flex items-center justify-center gap-8">
+				<div className="flex flex-col items-center justify-center gap-5 sm:flex-row md:gap-8">
 					<Button label="Demo anfordern" onPress="/demo" arrowed />
 					<a href="#features" className="underline">
 						Mehr erfahren
 					</a>
 				</div>
+				<Link to="/#features">
+					<img
+						src={ArrowDown}
+						alt="Scroll Indicator"
+						width={40}
+						className="absolute right-0 bottom-4 left-0 mx-auto animate-bounce md:hidden"
+					/>
+				</Link>
 			</div>
-			<div className="mb-12 flex gap-10">
+			{/* ClassInsights Highlights */}
+			<div className="mt-5 mb-12 flex w-3/4 flex-col items-center gap-10 md:mt-0 md:w-full md:flex-row">
 				{[
 					[
 						"Energieeffizienz",

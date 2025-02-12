f4e4c40d90b89e16543e68e70b1e1b3ce7a18942
akaJuliaan <info@versahl.at>
2025-02-12 12:36:07 +0100

    add config prop to window

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,9 @@
 import { ConfigData } from "./types/ConfigData";
 
+declare global {
+    interface Window { __CONFIG__: ConfigData | undefined; }
+}
+
 const env: Record<keyof ConfigData, undefined | string> = {
   VITE_API_URL: import.meta.env.VITE_API_URL,
   VITE_AUTH_URL: import.meta.env.VITE_AUTH_URL,
@@ -8,9 +12,9 @@ const env: Record<keyof ConfigData, undefined | string> = {
 
 // returns config value from environment or config.js
 function getValue(key: keyof ConfigData): string {
-  let windowValue = (window as any)?.__CONFIG__?.[key];
-  if (windowValue !== null && windowValue !== undefined && windowValue.length === 0) windowValue = undefined;
-  return env[key] ?? windowValue ?? "";
+  let windowValue = window.__CONFIG__?.[key];
+	if (windowValue !== null && windowValue !== undefined && windowValue.length === 0) windowValue = undefined;
+	return env[key] ?? windowValue ?? "";
 }
 
 export function conf(): ConfigData {

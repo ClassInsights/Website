87ef0ea50e94edd1de61a6970543b541ecc50100
Jakob <hey@jakob.foo>
2025-02-20 12:57:47 +0100

    feat: send access token on redirect

--- src/components/School.tsx
+++ src/components/School.tsx
@@ -1,8 +1,9 @@
 import { useCallback } from "react";
 import SettingsSVG from "../assets/svg/settings.svg?react";
 import { useSchoolModal } from "../contexts/SchoolContext";
-import { type SchoolData, translateRole } from "../types/SchoolData";
-import ExternalLink from "./ExternalLink";
+import { Role, type SchoolData, translateRole } from "../types/SchoolData";
+import ArrowSVG from "../assets/svg/arrow.svg?react";
+import { useAuth } from "../contexts/AuthContext";
 
 type SchoolProps = {
 	school: SchoolData;
@@ -12,23 +13,37 @@ type SchoolProps = {
 /** The School component with redirect to local dashboard */
 const School = ({ school, multiple }: SchoolProps) => {
 	const schoolModal = useSchoolModal();
+	const auth = useAuth();
 
 	const editSchool = useCallback(() => schoolModal.show(school), [school, schoolModal]);
+	const navigateToDashboard = useCallback(
+		() => location.replace(`${school.LocalDashboardUrl}?token=${auth.data?.access_token}`),
+		[school, auth.data],
+	);
 
 	return (
 		<div className={`w-full ${multiple ? "school" : "rounded-md border-2 border-[#F1F4FF] px-8 py-4"}`}>
 			<div className="flex items-start justify-between gap-2">
 				<h2 className="text-2xl">{school.Name}</h2>
-				<SettingsSVG
-					onClick={editSchool}
-					onKeyDown={editSchool}
-					width={26}
-					height={26}
-					className="mt-[0.2rem] shrink-0 cursor-pointer fill-primary"
-				/>
+				{school.Roles.includes(Role.ADMIN) && (
+					<SettingsSVG
+						onClick={editSchool}
+						onKeyDown={editSchool}
+						width={26}
+						height={26}
+						className="mt-[0.2rem] shrink-0 cursor-pointer fill-primary"
+					/>
+				)}
 			</div>
 			<p>Rolle: {school.Roles.map((role) => translateRole(role)).join(", ")}</p>
-			<ExternalLink href={school.LocalDashboardUrl} label="Zum Dashboard" newPage />
+			<div
+				className="mt-3 flex cursor-pointer items-center gap-1.5 text-primary"
+				onClick={navigateToDashboard}
+				onKeyDown={navigateToDashboard}
+			>
+				<p>Zum Dashboard</p>
+				<ArrowSVG className="shrink-0 fill-primary" />
+			</div>
 		</div>
 	);
 };

b7bb9e21bd4ddc984082a8aff1fedc1dcb60125d
Jakob <hey@jakob.foo>
2025-05-02 00:04:59 +0200

    fix: modal height

--- src/components/EditSchool.tsx
+++ src/components/EditSchool.tsx
@@ -21,7 +21,7 @@ const EditSchool = () => {
 				onClick={schoolModal.hide}
 				onKeyDown={schoolModal.hide}
 			/>
-			<div className="absolute h-[88%] w-full rounded-t-2xl bg-background p-4 md:h-auto md:w-10/12 md:rounded-2xl lg:w-4/6 xl:w-1/2 2xl:w-2/5">
+			<div className="absolute h-[88%] max-h-[88%] w-full overflow-hidden rounded-t-2xl bg-background p-4 pb-2 md:w-10/12 md:rounded-2xl lg:w-4/6 xl:w-1/2 2xl:w-2/5">
 				{/* Title Bar */}
 				<div className="flex items-start justify-between bg-background pb-2">
 					<CloseSVG className="shrink-0 opacity-0" />
@@ -67,7 +67,7 @@ const EditSchool = () => {
 						initialValue={data.website}
 						onChange={(value) => schoolModal.updateData({ ...data, website: value })}
 					/>
-					<div className="mt-20 flex justify-end pb-16 md:pb-0">
+					<div className="mt-8 flex justify-end pb-12">
 						<Button label="Speichern" onPress={() => schoolModal.save()} disabled={!schoolModal.hasChanges} />
 					</div>
 				</div>

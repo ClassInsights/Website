7c11fe658baf2a5510c347eecef35171d27cd9b2
Jakob <hey@jakob.foo>
2025-05-23 17:25:30 +0200

    feat: added certificate copy button

--- src/components/School.tsx
+++ src/components/School.tsx
@@ -1,9 +1,11 @@
 import { useCallback } from "react";
 import ArrowSVG from "../assets/svg/arrow.svg?react";
 import SettingsSVG from "../assets/svg/settings.svg?react";
+import LicenseSVG from "../assets/svg/license.svg?react";
 import { useAuth } from "../contexts/AuthContext";
 import { useSchoolModal } from "../contexts/SchoolContext";
 import { Role, type SchoolData, translateRole } from "../types/SchoolData";
+import { useToast } from "../contexts/ToastContext";
 
 type SchoolProps = {
 	school: SchoolData;
@@ -14,21 +16,40 @@ type SchoolProps = {
 const School = ({ school, multiple }: SchoolProps) => {
 	const schoolModal = useSchoolModal();
 	const auth = useAuth();
+	const toast = useToast();
+
+	const copyCertificateCode = useCallback(() => {
+		navigator.clipboard
+			.writeText(
+				`<a href="https://classinsights.at?sid=${school.SchoolId}" target="_blank" rel="noopener noreferrer"><img src="https://classinsights.at/certificate.svg" alt="ClassInsights Zertifikat von ${school.Name} auf classinsights.at anzeigen" width="75" height="75"></a>`,
+			)
+			.then(() => toast.showMessage("Zertifikat HTML Code kopiert"))
+			.catch(() => toast.showMessage("Kopieren fehlgeschlagen!", "error"));
+	}, [toast.showMessage, school.SchoolId, school.Name]);
 
 	const editSchool = useCallback(() => schoolModal.show(school), [school, schoolModal]);
 
 	return (
-		<div className={`w-full ${multiple ? "school" : "rounded-md border-2 border-[#F1F4FF] px-8 py-4"}`}>
+		<div className={`w-full ${multiple ? "school" : "rounded-md border-2 border-[#F1F4FF] px-4 py-2 lg:px-8 lg:py-4"}`}>
 			<div className="flex items-start justify-between gap-2">
 				<h2 className="text-2xl">{school.Name}</h2>
-				{school.Roles.includes(Role.ADMIN) && (
-					<SettingsSVG
-						onClick={editSchool}
-						onKeyDown={editSchool}
-						width={26}
-						height={26}
-						className="mt-[0.2rem] shrink-0 cursor-pointer fill-primary"
-					/>
+				{(school.Roles.includes(Role.ADMIN) || school.Roles.includes(Role.OWNER)) && (
+					<div className="mt-[0.2rem] flex gap-2">
+						<LicenseSVG
+							onClick={() => copyCertificateCode()}
+							onKeyDown={() => copyCertificateCode()}
+							width={25}
+							height={25}
+							className="shrink-0 cursor-pointer fill-primary"
+						/>
+						<SettingsSVG
+							onClick={editSchool}
+							onKeyDown={editSchool}
+							width={25}
+							height={25}
+							className="shrink-0 cursor-pointer fill-primary"
+						/>
+					</div>
 				)}
 			</div>
 			<p>

e3c2f86faac334765b99241681b534f65ef3ce26
Jakkoble <hey@jakob.foo>
2025-09-11 11:24:46 +0200

    fix: updated config entries

--- public/config.js
+++ public/config.js
@@ -1,9 +1,6 @@
 window.__CONFIG__ = {
-  VITE_AUTH_URL:
-    "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=79ee7d18-c7f0-4a81-a8b3-975fc5c846fa&response_type=code&redirect_uri=http://localhost:5173/login&response_mode=query&scope=https://graph.microsoft.com/.default&prompt=select_account",
-  VITE_API_URL: "https://classinsights.dev/api",
-  VITE_JWKS_URL: "https://classinsights.dev/.well-known/jwks.json",
-  VITE_LOGOUT_URL:
-    "https://login.microsoftonline.com/common/oauth2/logout?post_logout_redirect_uri=http://localhost:5173/login",
-  VITE_TURNSTILE_SITE_KEY: "0x4AAAAAABBbwJ5ndOm7M4b0",
+  VITE_AUTH_URL: "CHANGE_ME",
+  VITE_LOGOUT_URL: "CHANGE_ME",
+  VITE_API_URL: "CHANGE_ME",
+  VITE_JWKS_URL: "CHANGE_ME",
 };
